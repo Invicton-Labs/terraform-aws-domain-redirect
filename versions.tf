6c5b676ab68041e967d7bbbcc1bcb534327d0d0f
Kyle Kotowick <kotowick@invictonlabs.com>
2025-09-01 14:21:52 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.11.3"
+  required_version = ">= 1.11.3"
   required_providers {
     aws = {
       source                = "hashicorp/aws"
@@ -8,3 +8,4 @@ terraform {
     }
   }
 }
+
